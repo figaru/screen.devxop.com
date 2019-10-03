421aa463dbaf3b75c5a3be03c23af5bc690731c7
Daniel Abrantes <daniel.abrantes95@gmail.com>
2019-10-03 23:28:36 +0100

    init

---
+++ app/src/main/java/com/devxop/screen/MainActivity.java
@@ -0,0 +1,57 @@
+package com.devxop.screen;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView mWebView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        mWebView = new WebView(this);
+        //mWebView.loadUrl("http://devxop.ddns.net:3000/display");
+        mWebView.loadUrl("file:///android_asset/main.html");
+
+        mWebView.setInitialScale(1);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                // TODO Auto-generated method stub
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError er) {
+                Log.d("CERTIFICATE SSL", "SSL error! ignore handler acted.");
+                handler.proceed();
+                // Ignore SSL certificate errors
+            }
+        });
+
+        this.setContentView(mWebView);
+    }
+}
