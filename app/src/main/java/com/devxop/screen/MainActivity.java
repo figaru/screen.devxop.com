6ff266bd4ffd61dec4fa17fed2e5f37e5952d6d4
Daniel Abrantes <daniel.abrantes95@gmail.com>
2019-10-07 10:01:36 +0100

    screen update

--- app/src/main/java/com/devxop/screen/MainActivity.java
+++ app/src/main/java/com/devxop/screen/MainActivity.java
@@ -9,7 +9,9 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.webkit.CookieManager;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -21,37 +23,54 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         mWebView = new WebView(this);
-        mWebView.loadUrl("http://devxop.ddns.net:3000/display");
-        //mWebView.loadUrl("file:///android_asset/main.html");
+        mWebView.setWebViewClient(new myWebClient());
+        mWebView.setWebChromeClient(new WebChromeClient());
+        //mWebView.loadUrl("http://devxop.ddns.net:3000/display");
+
+
 
         mWebView.setInitialScale(1);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setMediaPlaybackRequiresUserGesture(false);
         mWebView.getSettings().setDomStorageEnabled(true);
+
         mWebView.getSettings().setLoadWithOverviewMode(true);
         mWebView.getSettings().setUseWideViewPort(true);
         mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
 
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView, true);
+        } else {
+            CookieManager.getInstance().setAcceptCookie(true);
+        }
 
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                // TODO Auto-generated method stub
-                super.onPageStarted(view, url, favicon);
-            }
-
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError er) {
-                Log.d("CERTIFICATE SSL", "SSL error! ignore handler acted.");
-                handler.proceed();
-                // Ignore SSL certificate errors
-            }
-        });
 
         this.setContentView(mWebView);
+
+        mWebView.loadUrl("file:///android_asset/main.html");
+    }
+
+
+    public class myWebClient extends WebViewClient
+    {
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError er) {
+            handler.proceed();
+            // Ignore SSL certificate errors
+        }
+
+
     }
 }
